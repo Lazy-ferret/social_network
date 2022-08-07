18bef1ac38c576571dfe0fb502ab09bdba0d5610
Lazy-ferret <a.veselov83@gmail.com>
2022-08-07 22:14:35 +0300

    Routes and components Dialogs, News, Music, Settings added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
+import Music from './components/Music/Music';
 import Navbar from './components/Navbar/Navbar';
+import News from './components/News/News';
 import Profile from './components/Profile/Profile';
+import Settings from './components/Settings/Settings';
 
 function App() {
   return (
-    <div className="app-wrapper">
+    <BrowserRouter>
 
-      <Header />
+      <div className="app-wrapper">
 
-      <Navbar />
+        <Header />
 
-      <Profile />
+        <Navbar />
 
-    </div>
+        <div className='app-wrapper-content'>
+          <Routes >
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+
+        </div>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
