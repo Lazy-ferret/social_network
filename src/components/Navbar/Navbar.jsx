335d8ae93daa4db578c5c2b8574828dbebc7632a
Lazy-ferret <a.veselov83@gmail.com>
2022-08-08 17:35:52 +0300

    NavLinks for NavBar links added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from './Navbar.module.css'
+
+const linkClassName  = NavData => NavData.isActive ? s.active : s.item  
 
 export default function Navbar() {
     return (
         <nav className={s.nav}>
-            <div className={`${s.item} ${s.active}`}><a href='/profile'>Profile</a></div>
-            <div className={s.item}><a href='/dialogs'>Messages</a></div>
-            <div className={s.item}><a href='/news'>News</a></div>
-            <div className={s.item}><a href='/music'>Music</a></div>
-            <div className={s.item}><a href='/settings'>Settings</a></div>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to='/profile' className={linkClassName}>Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/dialogs' className={linkClassName}>Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/news' className={linkClassName}>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/music' className={linkClassName}>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/settings' className={linkClassName}>Settings</NavLink>
+            </div>
         </nav>
     )
 }
