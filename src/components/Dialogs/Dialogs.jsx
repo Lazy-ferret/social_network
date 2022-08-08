b52e2925e2440337b1b072928fe0fb412d41cd50
Lazy-ferret <a.veselov83@gmail.com>
2022-08-08 18:04:22 +0300

    Dialogs markup and initial styles added

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,6 +5,27 @@ export default function Dialogs() {
 
     return (
 
-        <div className={s.content}> Dialogs</div>
+        <div className={s.dialogs}>
+            <div className={s.dialogs_items}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Alex
+                </div>
+                <div className={s.dialog}>
+                    Ksu
+                </div>
+                <div className={s.dialog}>
+                    Lisa
+                </div>
+                <div className={s.dialog}>
+                    Myshka
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you doing?</div>
+                <div className={s.message}>Oh shit</div>
+                
+            </div>
+        </div>
     )
 }
