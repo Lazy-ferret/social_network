a32d1802e7113a3c604309f1f395dedd19cf5d25
Lazy-ferret <a.veselov83@gmail.com>
2022-08-24 23:42:05 +0300

    React-redux installed, store connected

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,35 +1,53 @@
 import React from 'react'
 import { addMessageCreator, udpateNewMessageTextCreator } from '../../redux/dialogsReducer'
-import StoreContext from '../../storeContext'
 import Dialogs from './Dialogs'
+import { connect } from 'react-redux'
 
 
-const DialogsContainer = () => {
+// const DialogsContainer = () => {
 
-    return (
-        <StoreContext.Consumer>
-            {
-                (store) => {
-                    let state = store.getState().dialogsPage
+//     return (
+//         <StoreContext.Consumer>
+//             {
+//                 (store) => {
+//                     let state = store.getState().dialogsPage
 
-                    const onMessageSend = () => {
-                        store.dispatch(addMessageCreator())
-                    }
+//                     const onMessageSend = () => {
+//                         store.dispatch(addMessageCreator())
+//                     }
 
-                    const onMessageChange = (text) => {
-                        const action = udpateNewMessageTextCreator(text)
-                        store.dispatch(action)
-                    }
-                    return (
-                        <Dialogs
-                            udpateNewMessageText={onMessageChange}
-                            onMessageSend={onMessageSend}
-                            dialogsPage={state} />
-                    )
-                }
-            }
-        </StoreContext.Consumer>
-    )
+//                     const onMessageChange = (text) => {
+//                         const action = udpateNewMessageTextCreator(text)
+//                         store.dispatch(action)
+//                     }
+//                     return (
+//                         <Dialogs
+//                             udpateNewMessageText={onMessageChange}
+//                             onMessageSend={onMessageSend}
+//                             dialogsPage={state} />
+//                     )
+//                 }
+//             }
+//         </StoreContext.Consumer>
+//     )
+// }
+
+const mapStateToProps = (state) => {
+    return {
+        dialogsPage: state.dialogsPage
+
+    }
 }
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        udpateNewMessageTex: (text) => {
+            dispatch(udpateNewMessageTextCreator(text))
+        }, 
+        onMessageSend: () => { dispatch(addMessageCreator())}
+    }
+}
+
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs)
 
 export default DialogsContainer
--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,34 +1,57 @@
 import React from "react";
 import { addPostCreator, udpateNewPostTextCreator } from "../../../redux/profileReducer";
-import StoreContext from "../../../storeContext";
 import MyPosts from "./MyPosts";
+import { connect } from 'react-redux'
 
-function MyPostsContainer() {
 
-    return (
-        <StoreContext.Consumer>
-            {
-                (store) => {
-                    let state = store.getState()
+// function MyPostsContainer() {
 
-                    const addPost = () => {
-                        store.dispatch(addPostCreator())
-                    }
+//     return (
+//         <StoreContext.Consumer>
+//             {
+//                 (store) => {
+//                     let state = store.getState()
 
-                    const onPostChange = (text) => {
-                        const action = udpateNewPostTextCreator(text)
-                        store.dispatch(action)
-                    }
+//                     const addPost = () => {
+//                         store.dispatch(addPostCreator())
+//                     }
 
-                    return (<MyPosts
-                        udpateNewPostText={onPostChange}
-                        addPost={addPost}
-                        posts={state.profilePage.posts}
-                        newPostText={state.profilePage.newPostText} />)
-                }
-            }
-        </StoreContext.Consumer>
-    )
+//                     const onPostChange = (text) => {
+//                         const action = udpateNewPostTextCreator(text)
+//                         store.dispatch(action)
+//                     }
+
+//                     return (<MyPosts
+//                         udpateNewPostText={onPostChange}
+//                         addPost={addPost}
+//                         posts={state.profilePage.posts}
+//                         newPostText={state.profilePage.newPostText} />)
+//                 }
+//             }
+//         </StoreContext.Consumer>
+//     )
+// }
+
+const mapStateToProps = (state) => {
+    return {
+        posts: state.profilePage.posts,
+        newPostText: state.profilePage.newPostText
+
+    }
 }
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        udpateNewPostText: (text) => {
+            const action = udpateNewPostTextCreator(text)
+            dispatch(action)
+        },
+        addPost: () => {
+            dispatch(addPostCreator())
+        }
+    }
+}
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts)
 
 export default MyPostsContainer
