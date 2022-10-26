32bfe1b302cbc34c4fbaf9f86144c5d29458c4eb
Lazy-ferret <a.veselov83@gmail.com>
2022-10-26 21:40:01 +0300

    componentDidUpdate added to ProfileStatus component

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -11,7 +11,7 @@ class ProfileContainer extends React.Component {
 
     componentDidMount() {
         let userId = this.props.params.userId
-        if (!userId) { userId = 2 }
+        if (!userId) { userId =  2 }
         this.props.getUserProfile(userId);
         this.props.getStatus(userId)
     }
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -11,15 +11,16 @@ const ProfileInfo = (props) => {
     return (
         <div >
             <div>
-                <img src='https://avatars.mds.yandex.net/i?id=0d179fbb661a0c4b1caa54cce39e5054-5449619-images-thumbs&n=13&exp=1' alt=''/>
+                <img src='https://avatars.mds.yandex.net/i?id=0d179fbb661a0c4b1caa54cce39e5054-5449619-images-thumbs&n=13&exp=1' alt='' />
             </div>
 
             <div className={styles.description_block}>
-                <img src={props.profile.photos.large} alt='profile_photo'/>
+                <img src={props.profile.photos.large} alt='profile_photo' />
                 <div className={styles.description}>
+                    <div className={styles.name}>{props.profile.fullName}</div>
                     <div className={styles.about_me}>{props.profile.aboutMe}</div>
                     <div className={styles.contacts}></div>
-                    <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+                    <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
                 </div>
             </div>
 
