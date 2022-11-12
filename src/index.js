7c66472ad4a675c611161f99c026e0f2570705b5
Lazy-ferret <a.veselov83@gmail.com>
2022-11-13 00:21:32 +0300

    Hash pouter added, deploy on gh-pages added

--- src/index.js
+++ src/index.js
@@ -4,23 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/reduxStore'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
 root.render(
-    
-        <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>
-    
+
+    <HashRouter >
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </HashRouter>
+
 );
-
-
 
 // rerenderEntireTree()
 
